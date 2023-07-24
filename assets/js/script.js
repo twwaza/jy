9f33da9dbe6f17526d13bb337b1f7b35e2159e6b
twwaza <139862962+twwaza@users.noreply.github.com>
2023-07-24 16:55:34 +0700

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+<script>
+  document.getElementById("twwaza").style.fontSize = "14px"; 
+ document.getElementById("twwaza").style.color = "#";
+document.getElementById("twwaza").innerHTML = " Download Now";
+
+<!-- A -->
+
+document.getElementById("tak").style.fontSize = "15px"; 
+ document.getElementById("tak").style.color = "#198754"; 
+document.getElementById("tak").innerHTML = " @ 2023 M-66P";
+
+</script> 
