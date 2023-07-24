470946d09aeb3f96d947a3830900ef997b8c3c2b
twwaza <139862962+twwaza@users.noreply.github.com>
2023-07-24 17:32:36 +0700

    Create banner-down.js

---
+++ assets/js/banner-down.js
@@ -0,0 +1,6 @@
+document.getElementById("twwaza").style.fontSize = "14px"; 
+ document.getElementById("twwaza").style.color = "#198754";
+document.getElementById("twwaza").innerHTML = " Download Now";
+document.getElementById("tak").style.fontSize = "15px"; 
+ document.getElementById("tak").style.color = "#198754"; 
+document.getElementById("tak").innerHTML = " @ 2023 M-66P";
