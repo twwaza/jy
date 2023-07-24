e7580f667cd94e4803579cb85a20b7eb54864b48
twwaza <139862962+twwaza@users.noreply.github.com>
2023-07-24 17:22:14 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-<script>
-  document.getElementById("twwaza").style.fontSize = "14px"; 
+ document.getElementById("twwaza").style.fontSize = "14px"; 
  document.getElementById("twwaza").style.color = "#";
 document.getElementById("twwaza").innerHTML = " Download Now";
 
@@ -8,5 +7,3 @@ document.getElementById("twwaza").innerHTML = " Download Now";
 document.getElementById("tak").style.fontSize = "15px"; 
  document.getElementById("tak").style.color = "#198754"; 
 document.getElementById("tak").innerHTML = " @ 2023 M-66P";
-
-</script> 
